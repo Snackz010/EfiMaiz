c351354e8f23d255848eb490fc3512f6232a2f71
Klissman Espinoza <klissman010@gmail.com>
2019-12-01 16:40:22 -0600

    Cambiando la navegacion de caracterizacion del sitio

--- src/Opciones/CaracterizacionSitio/StackOpciones/StackOpciones.js
+++ src/Opciones/CaracterizacionSitio/StackOpciones/StackOpciones.js
@@ -1,7 +1,6 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 
-import InicioListaCs from '../Pantallas/InicioListaCS';
 import ListaCaracterizacionSitio from '../Pantallas/ListaCaracterizacionSitio.js';
 import ContenedorAgua from '../Pantallas/Agua/Contenedores/ContenedorAgua';
 import ContenedorClima from '../Pantallas/Clima/Contenedores/ContenedorClima';
@@ -9,20 +8,6 @@ import ContenedorSuelo from '../Pantallas/Suelo/Contenedores/ContenedorSuelo';
 import InicioListaCS from '../Pantallas/InicioListaCS';
 
 const ListaCaracterizacionSitioStack = createStackNavigator({
-    inicioLista:{
-        screen:InicioListaCS,
-        navigationOptions:{
-            header:null,
-        }
-    },
-    listaOpciones:{
-        screen:ListaCaracterizacionSitio,
-        headerModer:'none',
-        navigationOptions:{
-            headerShown:false,
-        }
-    },
-
     Clima:{
         screen:ContenedorClima,
         headerModer:'none',
