fd942ad4df0b575997e036d895ec05f537a0ef19
eliab51 <eliabjselvacruz51@gmail.com>
2019-12-08 00:58:17 -0600

    Modificando estilos del componente herbicidas.

--- src/Opciones/ManejoAgronomico/Contenedores/Herbicidas/ContenedorHerbicidas.js
+++ src/Opciones/ManejoAgronomico/Contenedores/Herbicidas/ContenedorHerbicidas.js
@@ -32,67 +32,67 @@ class ContenedorHerbicida extends Component {
       {
           title: 'Gesagard PH 50%',
           value: 'Gesagard PH 50%',
-          dosis: '1,5 - 2,0 kg/ha'
+          dosis: '1,5 - 2,0 kg / ha'
       },
       {
           title: 'Prometex PH 50% (Prometrina)',
           value: 'Prometex PH 50% (Prometrina)',
-          dosis: '1,5 - 2,0 kg/ha'
+          dosis: '1,5 - 2,0 kg / ha'
       },
       {
         title: 'Gesapax PH 80%',
         value: 'Gesapax PH 80%',
-        dosis: '2,0 - 2,5 kg/ha'
+        dosis: '2,0 - 2,5 kg / ha'
       },
       {
         title: 'Ametrex GD 80%',
         value: 'Ametrex GD 80%',
-        dosis: '2,0 - 2,5 kg/ha'
+        dosis: '2,0 - 2,5 kg / ha'
       },
       {
         title: 'Ametrol SC 50%',
         value: 'Ametrol SC 50%',
-        dosis: '2,0 - 2,5 kg/ha'
+        dosis: '2,0 - 2,5 kg / ha'
       },
       {
         title: 'Ametryn PH 80%',
         value: 'Ametryn PH 80%',
-        dosis: '2,0 - 2,5 kg/ha'
+        dosis: '2,0 - 2,5 kg / ha'
       },
       {
         title: 'Glifosato SC 48%',
         value: 'Glifosato SC 48%',
-        dosis: '1,44 -2,16 kg/ha'
+        dosis: '1,44 -2,16 kg / ha'
       },
       {
         title: 'Glifosato 36 SL',
         value: 'Glifosato 36 SL',
-        dosis: '3 - 3,5 L/mz'
+        dosis: '3 - 3,5 L / mz'
       },
       {
         title: 'Gramoxone',
         value: 'Gramoxone',
-        dosis: '2,0 - 2,5 L/Ha'
+        dosis: '2,0 - 2,5 L / Ha'
       },
       {
         title: 'Doblete',
         value: 'Doblete',
-        dosis: '1,0 - 2,0 L/Ha'
+        dosis: '1,0 - 2,0 L / Ha'
       },
       {
         title: 'Fusilade (Fluazitop-p-bulito)',
         value: 'Fusilade (Fluazitop-p-bulito)',
-        dosis: '2,5 - 3,5 L/Ha'
+        dosis: '2,5 - 3,5 L / Ha'
       },
       {
         title: 'Leopar CE 10.8%',
         value: 'Leopar CE 10.8%',
-        dosis: '2,5 - 3,5 L/Ha'
+        dosis: '2,5 - 3,5 L / Ha'
       },
       {
         title: 'Mizil CE 10%',
         value: 'Mizil CE 10%',
-        dosis: '2,5 - 3,5 L/Ha'
+        dosis: '2,5 - 3,5 L / Ha'
       },
   ]
 
